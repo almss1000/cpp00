e70320b7bc5d6ef3bd635b9c3e7f626fe45b563c
Amine Alimoussa <aalimous@e1r2p14.1337.ma>
2025-11-17 17:30:27 +0100

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,23 +5,13 @@ int main(int ac, char** argv)
 {
     if (ac == 1)
     {
-        printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *\n");
-        std::cout << "ali" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
-    int i = 0;
-    int k = 1;
-    while (argv[k])
-    {
-        i = 0;
-        while (argv[k][i])
-        {
-            if (argv[k][i] >= 97 && argv[k][i] <= 122)
-                argv[k][i] = argv[k][i] - 32;
-            i++;
-        }
-        printf("%s", argv[k]);
-        k++;
+    std::string str;
+    for (int k = 1; k < ac; k++) {
+        str = argv[k];
+        for (int i = 0; i < str.size(); i++)
+            std::cout << (char)std::toupper(str[i]);
     }
-    printf("\n");
 }
